1d9e365e95d164018753ddd0fd18223d2f5c705f
DakotaSlaySWF <dakota.g.slay.mil@swf.army.mil>
2022-03-18 10:59:52 -0500

    First commit

---
+++ src/test/java/com/galvalnize/Spring/Playground/SpringPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvalnize.Spring.Playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
